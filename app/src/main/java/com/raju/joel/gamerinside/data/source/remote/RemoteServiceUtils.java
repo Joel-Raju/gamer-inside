2dc60affed22a102d3dea55bc172e39126f555e9
Joel Raju <joelraju@ymail.com>
2017-11-11 23:19:22 +0530

    moved the IGDB API Key to Gradle Constant

--- app/src/main/java/com/raju/joel/gamerinside/data/source/remote/RemoteServiceUtils.java
+++ app/src/main/java/com/raju/joel/gamerinside/data/source/remote/RemoteServiceUtils.java
@@ -1,6 +1,8 @@
 package com.raju.joel.gamerinside.data.source.remote;
 
 
+import com.raju.joel.gamerinside.BuildConfig;
+
 /**
  * Created by Joel on 21-Sep-17.
  */
@@ -10,7 +12,7 @@ public class RemoteServiceUtils {
     public static final int STATUS_CODE_OK = 200;
 
     private static String getToken() {
-        return "d44cep123tqa2bni6j2fd39ea4e7ll";
+        return BuildConfig.IGDB_API_KEY;
     }
 
     public static RemoteService getRemoteService() {
